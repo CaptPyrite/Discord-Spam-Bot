bbf5ca052878351b46c0c1aa0bd93a4addd3b838
Fahim <79488582+FahimFerdous1@users.noreply.github.com>
2021-12-30 20:01:01 -0500

    Add files via upload

---
+++ Discord spam bot.py
@@ -0,0 +1,25 @@
+import requests
+import time
+import random
+import keyboard
+
+payload = {
+    "content":random.uniform(-1,1)
+}
+
+header = {
+    "authorization":"YOUR DISCORD AUTHORIZATION"
+}
+
+msg_count = 0
+
+print("press Ctrl+Q to quit")
+while keyboard.is_pressed("ctrl+q") != True:
+    if msg_count >= 5:
+        time.sleep(4.5)
+        msg_count = 0
+    else:
+        r = requests.post("CHANEL ID",data=payload,headers=header)
+        payload["content"] = random.uniform(-1,1)
+    msg_count += 1
+print(f"messages total spammed: {msg_count}")
